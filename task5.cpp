c8e79d1d426b288b5b65f67187a8d1fa50e9111f
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-27 20:08:56 +0300

    added task2

---
+++ task5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int A = 0, B = 0, C = 0, X0 = 0, Xi = 0;;
+	vector<int> arr1;
+	cout << "Введите диапозон значений С:\nC = ";
+	cin >> C;
+	cout << "Введите множитель A(0<=A<=C):\nA = " ;
+	cin >> A;
+	cout << "Введите инкрементируещее значений B(0<=B<=C):\nB = ";
+	cin >> B;
+	cout << "Введите начальное значений X0(0<=X0<=C):\nX0 = ";
+	cin >> X0;
+	arr1.emplace(arr1.begin(), X0);
+	for (int i = 1; i < C; i++)
+	{
+		Xi = (A * arr1[i-1]+B) % C;
+		arr1.emplace(arr1.begin()+i, Xi);
+	}
+	for (auto i : arr1)
+	{
+		cout << i << " ";
+	}
+
+
+	return 0;
+}
